c46fa3baa50255638139fc2b3a61d0932c5f7b24
xiaoyebukuang <xiaoxiaobukuang@126.com>
2018-09-17 16:30:33 +0800

    添加新项目

---
+++ qingsonghuan/qingsonghuan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qingsonghuan
+//
+//  Created by 陈晓 on 2018/9/17.
+//  Copyright © 2018年 XYBK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
